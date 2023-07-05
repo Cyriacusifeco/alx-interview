23cf817a0d441325c702d4fd565ed111a34572c3
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-07-05 16:24:59 +0000

    Add documentations

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""
+An algorithm to determine if a list of boxes can be unlocked
+based on the keys in the unlocked ones. Each box is numbered
+sequentially from 0 to n - 1 and each box may contain keys
+(index of the next box) to the other boxes.
+"""
 
 
 def canUnlockAll(boxes):
+    """
+    A method that returns True if all the boxes can be unlocked
+    and False otherwise.
+    """
     n = len(boxes)  # Number of boxes
     unlocked = [False] * n  # Track the state of each box
     unlocked[0] = True  # The first box is unlocked initially
