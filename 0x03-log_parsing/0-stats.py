6cb6a6b814d0eeaf174b79d6d2fc7ef96b74d5b0
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-07-20 16:21:46 +0000

    parse logs

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,10 +2,12 @@
 
 import sys
 
+
 def print_statistics(total_size, status_codes):
     print(f"File size: {total_size}")
     for code, count in sorted(status_codes.items()):
         print(f"{code}: {count}")
+
 
 def process_lines():
     total_size = 0
@@ -18,20 +20,22 @@ def process_lines():
             # Parse the line using the specified format
             parts = line.split()
 
-            if len(parts) != 9 or parts[4] != '"GET' or not parts[5].startswith("/projects/"):
+            if (len(parts) != 9 or
+                    parts[4] != '"GET' or
+                    not parts[5].startswith("/projects/")):
                 continue
 
             status_code = parts[7]
             file_size = int(parts[8])
-            #print(status_code)
 
             # Update total file size
             total_size += file_size
-            #print(total_size)
 
             # Update status code count
             if status_code.isdigit():
-                status_codes[status_code] = status_codes.get(status_code, 0) + 1
+                status_codes[status_code] = (
+                        status_codes.get(status_code, 0) + 1
+                        )
 
             # Print statistics after every 10 lines
             if i % 10 == 0:
@@ -42,5 +46,6 @@ def process_lines():
 
     # Print final statistics
     print_statistics(total_size, status_codes)
+
 
 process_lines()
