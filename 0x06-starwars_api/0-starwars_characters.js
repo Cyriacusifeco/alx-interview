15401bd10026a646358e83b063ec60ecef52c219
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-08-11 15:47:06 +0000

    get characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -8,9 +8,10 @@ if (!movieId) {
   process.exit(1);
 }
 
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+const filmsApiUrl = 'https://swapi-api.alx-tools.com/api/films/';
+const movieApiUrl = `${filmsApiUrl}${movieId}/`;
 
-request(apiUrl, (error, response, body) => {
+request(movieApiUrl, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
     process.exit(1);
@@ -22,22 +23,31 @@ request(apiUrl, (error, response, body) => {
   }
 
   const movieData = JSON.parse(body);
-  const characters = movieData.characters;
+  const charactersUrls = movieData.characters;
 
-  characters.forEach(characterUrl => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error('Error:', error);
-        return;
-      }
-
-      if (response.statusCode !== 200) {
-        console.error('API responded with status code:', response.statusCode);
-        return;
-      }
-
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-    });
-  });
+  fetchAndDisplayCharacters(charactersUrls, 0);
 });
+
+function fetchAndDisplayCharacters (characterUrls, index) {
+  if (index >= characterUrls.length) {
+    process.exit(0);
+  }
+
+  const characterUrl = characterUrls[index];
+  request(characterUrl, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      console.error('API responded with status code:', response.statusCode);
+      return;
+    }
+
+    const characterData = JSON.parse(body);
+    console.log(characterData.name);
+
+    fetchAndDisplayCharacters(characterUrls, index + 1);
+  });
+}
